7ccfcc61770ca5f6256ddcc96fb94df6d5c5473a
Anna Kim <kimspb85@mail.ru>
2022-02-26 10:41:39 +0300

    c0_done

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -13,10 +13,8 @@ def fib_recursive(n: int) -> int:
     if n < 0:
         raise ValueError
 
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
+    if n in (0, 1):
+        return n
 
     return fib_recursive(n - 1) + fib_recursive(n - 2)
 
@@ -39,4 +37,34 @@ def fib_iterative(n: int) -> int:
 
 def fib_generator(n: int) -> Iterator:
     a, b = 0, 1
-    return (a + b for _ in range(n) if n > 0)
+    i = 0
+    while i <= n:
+        a, b = b, a + b
+        res = a + b
+        i += 1
+    yield res
+
+
+def fib_generator_1(n: int) -> Iterator:
+    a, b = 0, 1
+    yield a
+    yield b
+
+    for i in range(0, n):
+        a, b = b, a + b
+        yield a
+
+class Fib_generator:
+    def __init__(self, n):
+        self.n = n
+
+    def __iter__(self):
+        self.a = 0
+        self.b = 1
+
+    def __next__(self):
+        fib_gen = self.a
+        if fib_gen >= self.n:
+            raise StopIteration
+        self.a, self.b = self.b, self.a + self.b
+        return fib_gen
