90ac5c903b2f92573c1c0cda9b8560ee774be550
Anna Kim <kimspb85@mail.ru>
2022-03-13 00:44:38 +0300

    e0, e1, f0

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,5 +1,21 @@
 from typing import Hashable, List
+from collections import deque
 import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def draw_graph(graph):
+    pos = nx.spring_layout(graph)
+    nx.draw_networkx_nodes(graph, pos)
+    nx.draw_networkx_labels(graph, pos)
+
+    for edge in graph.edges:
+        nx.draw_networkx_edges(
+            graph, pos,
+            edgelist=[(edge[0], edge[1])], connectionstyle="arc3,rad=0.2"
+        )
+
+    plt.show() #plt.savefig()
 
 
 def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
@@ -10,5 +26,44 @@ def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node for search
     :return: list of nodes in the visited order
     """
+    draw_graph(g)
+    path_nodes = []  # сгоревшие узлы по порядку их горения
+    visited_nodes = {node: False for node in
+                     g.nodes}  # посещенные узлы (которые уже сгорели полностью или которые еще горят)
+
+    wait_nodes = deque() # очередь из горящих узлов, ожидающих поджечь своих соседей
+    wait_nodes.append(start_node) # добавляем в конец очереди узел
+    visited_nodes[start_node] = True
+
+    while wait_nodes:
+        current_node = wait_nodes.popleft() # забираем горящий узел с начала очереди
+        neighbours = g[current_node]
+        for neighbour in neighbours:
+            if not visited_nodes[neighbour]:
+                wait_nodes.append(neighbour) # конец очереди справа
+                visited_nodes[neighbour] = True
+        path_nodes.append(current_node)
+
     print(g, start_node)
-    return list(g.nodes)
+    return path_nodes
+
+if __name__ == '__main__':
+    graph = nx.Graph()
+    graph.add_nodes_from("ABCDEFGHIJ")
+    graph.add_edges_from([
+        ('A', 'B'),
+        ('A', 'F'),
+        ('B', 'G'),
+        ('F', 'G'),
+        ('G', 'C'),
+        ('G', 'H'),
+        ('G', 'I'),
+        ('C', 'H'),
+        ('I', 'H'),
+        ('H', 'D'),
+        ('H', 'E'),
+        ('H', 'J'),
+        ('E', 'D'),
+    ])
+    draw_graph(graph)
+    print(bfs(graph, 'A'))
--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -1,14 +1,77 @@
 from typing import Hashable, List
+from collections import deque
+
 import networkx as nx
+import matplotlib.pyplot as plt
 
 
-def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
+def draw_graph(graph):
+    pos = nx.spring_layout(graph)
+    nx.draw_networkx_nodes(graph, pos)
+    nx.draw_networkx_labels(graph, pos)
+
+    for edge in graph.edges:
+        nx.draw_networkx_edges(
+            graph, pos,
+            edgelist=[(edge[0], edge[1])], connectionstyle="arc3,rad=0.2"
+        )
+
+    plt.show()  # plt.savefig()
+
+
+# def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
+#     """
+#     Do an breadth-first search and returns list of nodes in the visited order
+#
+#     :param g: input graph
+#     :param start_node: starting node for search
+#     :return: list of nodes in the visited order
+#     """
+#     draw_graph(g)
+#
+#     path_nodes = []
+#     visited_nodes = {node: False for node in g.nodes}  # посещенные узлы
+#
+#     wait_nodes = []  # стек
+#     wait_nodes.append(start_node)  # добавляем в стек  / вершина справа
+#     visited_nodes[start_node] = True
+#
+#     while wait_nodes:
+#         current_node = wait_nodes.pop()
+#         neighbours = g[current_node]
+#
+#         for neighbour in neighbours:
+#             if not visited_nodes[neighbour]:
+#                 wait_nodes.append(neighbour)
+#                 visited_nodes[neighbour] = True
+#
+#         path_nodes.append(current_node)
+#
+#     return path_nodes
+
+
+def dfs(g: nx.Graph, start_node: Hashable):
     """
-    Do an depth-first search and returns list of nodes in the visited order
-
-    :param g: input graph
-    :param start_node: starting node of search
-    :return: list of nodes in the visited order
+    от каждого соседа рекурсивно запустить поиск в глубину,
+    то есть по факту сосед становится новым стартовым узлом.
+    Подумать:
+    - что должна возвращать рекурсивная функция
+    - какой терминальный случай
+    - как контролировать какие узлы уже посещали, чтобы дважды не пройти по тем же узлам
     """
-    print(g, start_node)
-    return list(g.nodes)
+    draw_graph(g)
+    path_nodes = []
+    visited_nodes = {node: False for node in g.nodes}
+
+    def _dfs(current_node: Hashable):
+        path_nodes.append(current_node)
+        visited_nodes[current_node] = True
+
+        neighbours = g[current_node]
+        for neighbour in neighbours:
+            if not visited_nodes[neighbour]:
+                _dfs(neighbour)
+
+    _dfs(start_node)
+
+    return path_nodes
